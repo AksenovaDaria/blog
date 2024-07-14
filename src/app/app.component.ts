122c49bdb795d7046a8e7c2ea7490c782c2eeb2a
Аксенова Дарья <aksenova@lanit.ru>
2024-07-14 16:24:10 +0300

    add primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { loadArticals, loadCategories } from './store/articals/artical.action';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'blog';
 
   constructor(
     private readonly store$: Store<any>
